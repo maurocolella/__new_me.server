e3994beef642715e9c37e50132371ba963220391
Mauro Colella <mauro.ludovico.colella@gmail.com>
2017-09-11 12:05:36 +0800

    Add articles controller

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use App\Article;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,29 +19,8 @@ use App\Article;
 	return $request->user();
 }); */
 
-Route::get('articles', function() {
-	// If the Content-Type and Accept headers are set to 'application/json',
-	// this will return a JSON structure. This will be cleaned up later.
-	return Article::all();
-});
-
-Route::get('articles/{id}', function($id) {
-	return Article::find($id);
-});
-
-Route::post('articles', function(Request $request) {
-	return Article::create($request->all);
-});
-
-Route::put('articles/{id}', function(Request $request, $id) {
-	$article = Article::findOrFail($id);
-	$article->update($request->all());
-
-	return $article;
-});
-
-Route::delete('articles/{id}', function($id) {
-	Article::find($id)->delete();
-
-	return 204;
-});
+Route::get('articles', 'ArticleController@index');
+Route::get('articles/{article}', 'ArticleController@show');
+// Route::post('articles', 'ArticleController@store');
+// Route::put('articles/{article}', 'ArticleController@update');
+// Route::delete('articles/{article}', 'ArticleController@delete');
