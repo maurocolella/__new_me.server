72797c848b5274f0f4e3855a95252db03902f1d0
Mauro Colella <mauro.ludovico.colella@gmail.com>
2017-09-12 09:26:59 +0800

    Alter model to show single article by slug

--- app/Article.php
+++ app/Article.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-	protected $fillable = ['title', 'body'];
+	protected $fillable = ['title', 'slug', 'body'];
+
+	public function getRouteKeyName()
+	{
+		return 'slug';
+	}
 }
