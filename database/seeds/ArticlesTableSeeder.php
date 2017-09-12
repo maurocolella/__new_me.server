b3118e24f682acc5a8b8a0fd48659d8c29616b15
Mauro Colella <mauro.ludovico.colella@gmail.com>
2017-09-12 09:22:01 +0800

    Add slug field (articles) and modify seeder

--- database/seeds/ArticlesTableSeeder.php
+++ database/seeds/ArticlesTableSeeder.php
@@ -13,9 +13,10 @@ class ArticlesTableSeeder extends Seeder
 		$faker = \Faker\Factory::create();
 
 		// And now, let's create a few articles in our database:
-		for ($i = 0; $i < 50; $i++) {
+		for ($i = 0; $i < 5; $i++) {
 			Article::create([
 				'title' => $faker->sentence,
+				'slug' => $faker->word,
 				'body' => $faker->paragraph,
 			]);
 		}
