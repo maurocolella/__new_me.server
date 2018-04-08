7679bf099412d93a3c5cdbff5c076c0d4ba1a1b3
Mauro Colella <mauro.ludovico.colella@gmail.com>
2018-04-08 18:56:26 +0800

    Order skills by descending rating

--- app/Http/Controllers/SkillController.php
+++ app/Http/Controllers/SkillController.php
@@ -10,7 +10,7 @@ class SkillController extends Controller
 {
 	public function index()
 	{
-		return Skill::all();
+		return Skill::orderBy('rating', 'desc')->get();
 	}
 
 	public function show(Skill $skill)
