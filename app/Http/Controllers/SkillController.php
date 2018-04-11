108c8071976ffa5f86023abe9c8eedbaeb69f946
Mauro Colella <mauro@mytenten.com>
2018-04-11 14:28:46 +0800

    Format according to json api

--- app/Http/Controllers/SkillController.php
+++ app/Http/Controllers/SkillController.php
@@ -3,39 +3,73 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 use App\Skill;
 
 class SkillController extends Controller
 {
-    public function index()
-    {
-        return Skill::orderBy('title', 'asc')->get();
-    }
+	public function index()
+	{
+		$payload = new \stdClass;
+		$skills = Skill::orderBy('title', 'asc')->get();
+		$payload->data = array();
 
-    public function show(Skill $skill)
-    {
-        return $skill;
-    }
+		$skills->each(function ($item, $key) use ($payload) {
+			$entry = new \stdClass;
+			$entry->type = 'skill';
+			$entry->id = $item->id;
+			$entry->attributes = new \stdClass;
 
-    public function store(Request $request)
-    {
-        $skill = Skill::create($request->all());
+			foreach($item->toArray() as $propName => $propVal){
+				if(strcmp('id', $propName) !== 0){
+					$entry->attributes->$propName = $propVal;
+				}
+			}
 
-        return response()->json($skill, 201);
-    }
+			$payload->data[] = $entry;
+		});
 
-    public function update(Request $request, Skill $skill)
-    {
-        $skill->update($request->all());
+		return (new Response(json_encode($payload), 200))
+			->header('Content-Type', 'application/vnd.api+json');
+	}
 
-        return response()->json($skill, 200);
-    }
+	public function show(Skill $skill)
+	{
+		$payload = new \stdClass;
+		$payload->data = new \stdClass;
+		$payload->data->type = 'skill';
+		$payload->data->id = $skill->id;
+		$payload->data->attributes = new \stdClass;
 
-    public function delete(Skill $skill)
-    {
-        $skill->delete();
+		foreach($skill->toArray() as $propName => $propVal){
+			if(strcmp('id', $propName) !== 0){
+				$payload->data->attributes->$propName = $propVal;
+			}
+		}
 
-        return response()->json(null, 204);
-    }
+		return (new Response(json_encode($payload), 200))
+			->header('Content-Type', 'application/vnd.api+json');
+	}
+
+	public function store(Request $request)
+	{
+		$skill = Skill::create($request->all());
+
+		return response()->json($skill, 201);
+	}
+
+	public function update(Request $request, Skill $skill)
+	{
+		$skill->update($request->all());
+
+		return response()->json($skill, 200);
+	}
+
+	public function delete(Skill $skill)
+	{
+		$skill->delete();
+
+		return response()->json(null, 204);
+	}
 }
